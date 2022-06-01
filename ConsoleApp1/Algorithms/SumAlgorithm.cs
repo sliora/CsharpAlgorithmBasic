8d16cd85aa65b69f911799c9478b316561c1fb02
zeromH <cupdisin@gmail.com>
2022-06-01 20:19:19 +0900

    Initail Commit

---
+++ ConsoleApp1/Algorithms/SumAlgorithm.cs
@@ -0,0 +1,29 @@
+﻿//[?] n명의 국어 점수 중에서 80점 이상인 점수의 합계
+using System;
+using System.Linq;
+/// <summary>
+/// 합계 알고리즘(Sum Algorithm) : 주어진 범위에 주어진 조건에 해당하는 자료들의 합계
+/// </summary>
+class SumAlgorithm
+{
+    static void Main()
+    {
+        //[1] Input n명의 국어 점수
+        int[] scores = { 100, 75, 50, 37, 90, 95 };
+        int sum = 0;
+
+        //[2] Process : 합계 알고리즘 영역 : 주어진 범위에 주어진 조건(필터링)
+        for (int i = 0; i < scores.Length; i++)
+        {
+            if (scores[i] >= 80)
+            {
+                sum += scores[i];
+            }
+        }
+        //실무용
+        //sum = scores.Where(s => s >= 80).Sum();
+
+        //[3] Output
+        Console.WriteLine($"{scores.Length} 명의 점수 중 80점 이상의 총점: {sum}");
+    }
+}
