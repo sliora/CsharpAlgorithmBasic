53b6614d6454ff360004712d94c3eb1833de2dca
zeromH <cupdisin@gmail.com>
2022-06-05 20:19:19 +0900

    카운트 알고리즘 기초

---
+++ ConsoleApp1/Algorithms/CountAlgorithm/CountAlgorithm.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+/// <summary>
+/// [?] 1부터 1000까지의 정수 중 13의 배수의 개수
+/// 개수 알고리즘(Count Algorithm) : 주어진 범위에 주어진 조건에 해당하는 자료들의 개수
+/// </summary>
+
+namespace ConsoleApp1.Algorithms.CountAlgorithm
+{
+    class CountAlgorithm
+    {
+        static void Main()
+        {
+            //[1] Input
+            int count = 0;
+
+            //[2] Process
+            for (int i = 1; i <= 1000; i++)
+            {
+                if (i % 13 == 0)
+                {
+                    count++;
+                }
+            }
+
+            //실무용
+            Enumerable.Range(1, 1_000).ToArray().Where(n => n % 13 == 0).Count();
+            Enumerable.Range(1, 1_000).ToArray().Count(n => n % 13 == 0);
+
+            //[3] Output
+            Console.WriteLine($"1부터 1,000까지의 정수 중 13의 배수의 개수 : {count}");
+            
+        }
+    }
+}
---
+++ ConsoleApp1/Algorithms/CountAlgorithm/소수.cs
@@ -0,0 +1,39 @@
+﻿using System;
+/// <summary>
+/// 소수 : 
+/// - 자연수 중에서 1과 자기 자신만을 약수로 가지는 자연수
+/// - 5는 소수 : 1과 5만을 약수로 가짐
+/// - 6은 소수가 아님 : 1, 3, 6을 약수로 가지므로
+/// </summary>
+
+namespace ConsoleApp1.Algorithms.CountAlgorithm
+{
+    class PrimeNumber
+    {
+        static void Main()
+        {
+            int k = 1;
+            int num = 0;
+            Console.Write("수 입력: _\b");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            do
+            {
+                k++;
+                Console.WriteLine("{0} % {1} = {2}", num, k, num % k);
+
+            } while (num % k != 0);
+
+            if (num == k)
+            {
+                Console.WriteLine("소수임");
+            }
+            else
+            {
+                Console.WriteLine("소수아님");
+            }
+
+
+        }
+    }
+}
