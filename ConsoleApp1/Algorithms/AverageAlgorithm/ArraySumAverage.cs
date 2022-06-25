cd7d38f8dbb3d9702f2cd4e7bf6cbc97b8ca28ec
zeromH <cupdisin@gmail.com>
2022-06-25 20:19:19 +0900

    ArraySumAverage Add

---
+++ ConsoleApp1/Algorithms/AverageAlgorithm/ArraySumAverage.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Algorithms.AverageAlgorithm
+{
+    class ArraySumAverage
+    {
+        static void Main(string[] args)
+        {
+            int[,] scores = 
+            {
+                { 90, 100, 0, 0},
+                { 80, 90, 0, 0 }, 
+                { 100, 80, 0, 0 }
+            };
+
+            for (int i = 0; i < 3; i++)
+            {
+                scores[i, 2] = scores[i, 0] + scores[i, 1]; // SUM
+                scores[i, 3] = scores[i, 2] / 2; //AVERAGE
+            }
+
+            Console.WriteLine("국어 영어 합계 평균");
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    Console.Write($"{scores[i, j], 4} ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ ConsoleApp1/Algorithms/AverageAlgorithm/평균이상학생수.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/// <summary>
+/// 5명의 학생의 토익 점수를 입력 받아서 전체 학생 평균 이상의 성적인 학생의 수를 구하는 프로그램
+/// 평균(합계, 카운트) -> 카운트 
+/// </summary>
+
+namespace ConsoleApp1.Algorithms.AverageAlgorithm
+{
+    class 평균이상학생수
+    {
+        static void Main(string[] args)
+        {
+            int[] t = {100, 90, 80, 70, 60};
+
+            int sum = 0; //총점
+            double avg = 0.0; //평균
+            int count = 0; //학생 수(평균 이상)
+
+            for (int i = 0; i < t.Length; i++)
+            {
+                sum += t[i];
+            }
+
+            avg = sum / (double)t.Length;
+
+            Console.WriteLine($"학생의 평균은 : {avg}");
+
+            for (int i = 0; i < t.Length; i++)
+            {
+                if (avg <= t[i])
+                {
+                    count++;
+                }
+            }
+
+            Console.WriteLine($"평균 이상의 학생 수는 : {count}");
+        }
+    }
+}
